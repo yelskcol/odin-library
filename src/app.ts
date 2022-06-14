35670e7f7acedbe17ea87dd545e1f8ee3e2b81ae
yelskcol <yelskcol@proton.me>
2022-06-14 03:26:02 +0100

    added logic

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,40 @@
-const stringOutputter = (text: string) => {
-  return text;
-};
+class Library {
+  list;
+  constructor() {
+    this.list = [];
+  }
 
-alert(stringOutputter("Hello!"));
+  static createFilm(title: string, year: string, director: string) {
+    const film = new Film(title, year, director);
+    return film;
+  }
+
+  addFilm(film: Film) {
+    this.list.push(film);
+  }
+}
+
+class Film {
+  constructor(
+    public title: string,
+    public releaseDate: string,
+    public director: string
+  ) {}
+}
+
+class App {
+  static init() {
+    const library = new Library();
+    library.addFilm(
+      Library.createFilm(
+        "Lord of the Rings: The Fellowship of the Ring",
+        "2001",
+        "Peter Jackson"
+      )
+    );
+
+    console.log(library.list);
+  }
+}
+
+App.init();
